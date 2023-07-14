8db577b2b0a1de0d7ee4e3b31635f88d8bc1fc63
AshlinHarris <ashlin_harris@brown.edu>
2023-07-14 10:21:55 -0400

    Update README

--- src/ETD.jl
+++ src/ETD.jl
@@ -1,16 +1,3 @@
-# Program based on joint COMS 7100 Final Project by Toheeb Biala and Ashlin Harris
-
-# This program uses the exponential time differencing (ETD) scheme to obtain 
-# solutions to the fractional reaction diffusion equation.
-# The space derivatives are discretized using the matrix Transfer Technique
-# developed by  [1]. In time, we use the ETD schemes given in [2].
-# [1] Ilic, M.; Liu, F.; Turner, I. & Anh, V. Numerical approximation of a fractional-in-space 
-#     diffusion equation (II)- with nonhomogenous boundary conditions Fractional Calculus and 
-#     Applied Analysis, 2006.
-# [2] Kleefeld, B.; Khaliq, A. & Wade, B. An ETD Crank-Nicolson method for reaction-diffusion 
-#     systems Numerical Methods for Partial Differential Equations, Wiley Online Library, 2012,
-#     28, 1309-1335
-
 @info "Loading modules..."
 using LinearAlgebra
 using Plots
